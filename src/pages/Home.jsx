adea6c4a8be055bf25d8ae9be1d7e4b49f63bee6
adithya123987 <adithyaptr77@gmail.com>
2026-01-31 11:13:58 +0530

    added navigation in navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Home() {
   const navigate = useNavigate();
 
   return (
     <div className="card">
+      <Navbar />
       <h2>Welcome Home 🎉</h2>
       <p>You are successfully logged in.</p>
 
